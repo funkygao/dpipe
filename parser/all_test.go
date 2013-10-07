27b9d48d2ce41b75f3264a0ab55777d446710159
funkygao <funky.gao@gmail.com>
2013-10-07 19:50:09 +0800

    extract log info and test cases

--- parser/all_test.go
+++ parser/all_test.go
@@ -31,3 +31,19 @@ func TestDefaultParserParseLine(t *testing.T) {
 	}
 	assert.Equal(t, expectedScriptId, scriptId)
 }
+
+func TestExtractLogInfo(t *testing.T) {
+	line := `us,1381118458069,{"cheater":10301051,"type":"helpFriendsRewardAction","world_id":"100001823535095","user":"100001823535095","_log_info":{"uid":10301051,"script_id":3183040714,"serial":3,"host":"10.255.8.189","ip":"79.215.100.157"}}`
+	p := new(DefaultParser)
+	_, _, data := p.ParseLine(line)
+	info := extractLogInfo(data)
+	assert.Equal(t, info.host, "10.255.8.189")
+	assert.Equal(t, info.ip, "79.215.100.157")
+	var (
+		expectedScriptId int64 = 3183040714
+		expectedUid int64 = 10301051
+	)
+	assert.Equal(t, expectedScriptId, info.scriptId)
+	assert.Equal(t, info.serial, 3)
+	assert.Equal(t, expectedUid, info.uid)
+}
--- parser/default.go
+++ parser/default.go
@@ -7,6 +7,38 @@ import (
 	"strconv"
 )
 
+type logInfo struct {
+	uid int64
+	scriptId int64
+	serial int
+	host string
+	ip string
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// extract _log_info fields from a log data entry
+func extractLogInfo(data *json.Json) logInfo {
+	var err error
+	info := logInfo{}
+	infoBody := data.Get("_log_info")
+	info.uid, err = infoBody.Get("uid").Int64()
+	checkError(err)
+	info.scriptId, err = infoBody.Get("script_id").Int64()
+	checkError(err)
+	info.serial, err = infoBody.Get("serial").Int()
+	checkError(err)
+	info.host, err = infoBody.Get("host").String()
+	checkError(err)
+	info.ip, err = infoBody.Get("ip").String()
+	checkError(err)
+	return info
+}
+
 func (this DefaultParser) ParseLine(line string) (area string, ts uint64, data *json.Json) {
 	fields := strings.SplitN(line, LINE_SPLITTER, LINE_SPLIT_NUM)
 
