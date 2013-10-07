baf7d9aa3a71febe2ab4efa77e7145c0965fb596
funkygao <funky.gao@gmail.com>
2013-10-07 20:18:53 +0800

    combine const and global file into one

--- global.go
+++ global.go
@@ -8,3 +8,21 @@ var (
 	options *Option
 	logger *log.Logger
 )
+
+const (
+	version = "0.1.a"
+	author = "gaopeng"
+)
+
+const (
+	LOG_OPTIONS = log.LstdFlags | log.Lshortfile
+	LOG_OPTIONS_DEBUG = log.Ldate | log.Lshortfile | log.Ltime | log.Lmicroseconds
+)
+
+const (
+	usage = `alser - FunPlus ALS log guard
+
+Flags:
+`
+	lockfile = "var/alser.lock"
+)
