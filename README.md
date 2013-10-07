6f6b5b5d0f0ce0689ec0ce7b1f9e540ca01aa645
funkygao <funky.gao@gmail.com>
2013-10-07 18:11:26 +0800

    CI with https://travis-ci.org/

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ alser
 =====
 
 ALS guard
+
+[![Build Status](https://secure.travis-ci.org/funkygao/alser.png)](http://travis-ci.org/funkygao/alser)
---
+++ parser/memcache.go
@@ -0,0 +1,13 @@
+// memcache set fail guard
+
+package parser
+
+import (
+	json "github.com/bitly/go-simplejson"
+)
+
+func (this MemcacheFailParser) ParseLine(line string) (area string, ts uint64, data *json.Json) {
+	area, ts, data = this.DefaultParser.ParseLine(line)
+
+	return
+}
