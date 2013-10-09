2cf517e8c21eb9aa67a6346d10c9065de8ae7a67
funkygao <funky.gao@gmail.com>
2013-10-09 13:53:15 +0800

    complaint with _log_info nil column

--- parser/util.go
+++ parser/util.go
@@ -1,37 +1,58 @@
 package parser
 
 import (
-    json "github.com/bitly/go-simplejson"
+	json "github.com/bitly/go-simplejson"
 )
 
 func checkError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 type logInfo struct {
-    uid      int64
-    scriptId int64
-    serial   int
-    host     string
-    ip       string
+	uid      int64
+	scriptId int64
+	serial   int
+	host     string
+	ip       string
 }
 
 // extract _log_info fields from a log data entry
 func extractLogInfo(data *json.Json) logInfo {
-    var err error
-    info := logInfo{}
-    infoBody := data.Get("_log_info")
-    info.uid, err = infoBody.Get("uid").Int64()
-    checkError(err)
-    info.scriptId, err = infoBody.Get("script_id").Int64()
-    checkError(err)
-    info.serial, err = infoBody.Get("serial").Int()
-    checkError(err)
-    info.host, err = infoBody.Get("host").String()
-    checkError(err)
-    info.ip, err = infoBody.Get("ip").String()
-    //checkError(err)
-    return info
+	var err error
+	info := logInfo{}
+	infoBody := data.Get("_log_info")
+
+	uid := infoBody.Get("uid")
+	if uid != nil {
+		info.uid, err = uid.Int64()
+		checkError(err)
+	}
+
+	scriptId := infoBody.Get("script_id")
+	if scriptId != nil {
+		info.scriptId, err = scriptId.Int64()
+		checkError(err)
+	}
+
+    serial := infoBody.Get("serial")
+    if serial != nil {
+        info.serial, err := serial.Int()
+        checkError(err)
+    }
+	
+    host := infoBody.Get("host")
+    if host != nil {
+        info.host, err = host.String()
+        checkError(err)
+    }
+	
+    ip := infoBody.Get("ip")
+    if ip != nil {
+        info.ip, err = ip.String()
+        checkError(err)
+    }
+	
+	return info
 }
