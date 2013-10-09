6a11a03f5a41243647ae893d4223c187332fb889
funkygao <funky.gao@gmail.com>
2013-10-09 11:35:40 +0800

    tuned on prod server

--- guard.go
+++ guard.go
@@ -24,82 +24,81 @@
 package main
 
 import (
-    //"github.com/funkygao/alser/parser"
-    "./parser"
-    "github.com/funkygao/gofmt"
-    "path/filepath"
-    "runtime"
-    "sync"
-    "time"
+	"github.com/funkygao/alser/parser"
+	"github.com/funkygao/gofmt"
+	"path/filepath"
+	"runtime"
+	"sync"
+	"time"
 )
 
 func guard(jsonConfig jsonConfig) {
-    parser.SetLogger(logger)
-    parser.SetVerbose(options.verbose)
-    parser.SetDebug(options.debug)
+	parser.SetLogger(logger)
+	parser.SetVerbose(options.verbose)
+	parser.SetDebug(options.debug)
 
-    var lines int = 0
-    var workerN int = 0
-    var wg = new(sync.WaitGroup)
-    chLines := make(chan int)
-    chAlarm := make(chan parser.Alarm, 1000)
+	var lines int = 0
+	var workerN int = 0
+	var wg = new(sync.WaitGroup)
+	chLines := make(chan int)
+	chAlarm := make(chan parser.Alarm, 1000)
 
-    // create all parsers at once
-    parser.NewParsers(jsonConfig.parsers(), chAlarm)
+	// create all parsers at once
+	parser.NewParsers(jsonConfig.parsers(), chAlarm)
 
-    for _, item := range jsonConfig {
-        paths, err := filepath.Glob(item.Pattern)
-        if err != nil {
-            panic(err)
-        }
+	for _, item := range jsonConfig {
+		paths, err := filepath.Glob(item.Pattern)
+		if err != nil {
+			panic(err)
+		}
 
-        for _, logfile := range paths {
-            workerN++
-            wg.Add(1)
-            go run_worker(logfile, item, wg, chLines)
-        }
-    }
+		for _, logfile := range paths {
+			workerN++
+			wg.Add(1)
+			go run_worker(logfile, item, wg, chLines)
+		}
+	}
 
-    if options.tick > 0 {
-        ticker = time.NewTicker(time.Second * time.Duration(options.tick))
-        go runTicker(&lines)
-    }
+	if options.tick > 0 {
+		ticker = time.NewTicker(time.Second * time.Duration(options.tick))
+		go runTicker(&lines)
+	}
 
-    logger.Println(workerN, "workers started")
+	logger.Println(workerN, "workers started")
 
-    go runAlarmCollector(chAlarm)
+	go runAlarmCollector(chAlarm)
 
-    // wait for all workers finish
-    go func() {
-        wg.Wait()
-        logger.Println("all", workerN, " workers finished")
+	// wait for all workers finish
+	go func() {
+		wg.Wait()
+		logger.Println("all", workerN, " workers finished")
 
-        close(chLines)
-        close(chAlarm)
-    }()
+		close(chLines)
+		close(chAlarm)
+	}()
 
-    // collect how many lines scanned
-    for l := range chLines {
-        lines += l
-    }
+	// collect how many lines scanned
+	for l := range chLines {
+		lines += l
+	}
 
-    parser.StopAll()
+	parser.StopAll()
 
-    logger.Println("all lines scaned:", lines)
+	logger.Println("all lines scaned:", lines)
 }
 
 func runTicker(lines *int) {
-    ms := new(runtime.MemStats)
-    for _ = range ticker.C {
-        runtime.ReadMemStats(ms)
-        logger.Printf("goroutine: %d, mem: %s, lines: %d\n",
-            runtime.NumGoroutine(), gofmt.ByteSize(ms.Alloc), *lines)
-    }
+	ms := new(runtime.MemStats)
+	for _ = range ticker.C {
+		runtime.ReadMemStats(ms)
+		logger.Printf("goroutine: %d, mem: %s, lines: %d\n",
+			runtime.NumGoroutine(), gofmt.ByteSize(ms.Alloc), *lines)
+	}
 }
 
 func runAlarmCollector(ch <-chan parser.Alarm) {
-    alarmLogger := newAlarmLogger()
-    for alarm := range ch {
-        alarmLogger.Println(alarm)
-    }
+	alarmLogger := newAlarmLogger()
+	for alarm := range ch {
+		alarmLogger.Println(alarm)
+	}
 }
--- worker.go
+++ worker.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-    "bufio"
-    //"github.com/funkygao/alser/parser"
-    "./parser"
-    "io"
-    "os"
-    "sync"
-    "time"
+	"bufio"
+	"github.com/funkygao/alser/parser"
+	"io"
+	"os"
+	"sync"
+	"time"
 )
 
 // Each single log file is a worker
 func run_worker(logfile string, conf jsonItem, wg *sync.WaitGroup, chLines chan int) {
-    defer wg.Done()
+	defer wg.Done()
 
-    file, err := os.Open(logfile)
-    if err != nil && err != os.ErrExist {
-        // sometimes logs may be rotated, so ErrExist is common
-        panic(err)
-    }
-    defer file.Close()
+	file, err := os.Open(logfile)
+	if err != nil && err != os.ErrExist {
+		// sometimes logs may be rotated, so ErrExist is common
+		panic(err)
+	}
+	defer file.Close()
 
-    if options.verbose {
-        logger.Printf("%s started with %v", logfile, conf.Parsers)
-    }
+	if options.verbose {
+		logger.Printf("%s started with %v", logfile, conf.Parsers)
+	}
 
-    reader := bufio.NewReader(file)
-    for {
-        line, _, err := reader.ReadLine()
-        if err != nil {
-            if err == io.EOF {
-                if options.tailmode {
-                    time.Sleep(time.Second * time.Duration(tailSleep))
-                    continue
-                } else {
-                    break
-                }
-            } else {
-                panic(err)
-            }
-        }
+	reader := bufio.NewReader(file)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				if options.tailmode {
+					time.Sleep(time.Second * time.Duration(tailSleep))
+					continue
+				} else {
+					logger.Printf("%s error occured: %s\n", logfile, err)
+					break
+				}
+			} else {
+				panic(err)
+			}
+		}
 
-        // a valid line scanned
-        chLines <- 1
-        for _, p := range conf.Parsers {
-            parser.Dispatch(p, string(line))
-        }
-    }
+		// a valid line scanned
+		chLines <- 1
+		for _, p := range conf.Parsers {
+			parser.Dispatch(p, string(line))
+		}
+	}
 
-    logger.Println(logfile, "finished")
+	logger.Println(logfile, "finished")
 }
