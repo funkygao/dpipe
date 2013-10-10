3fceb5ec1ebf7467980a16b80804ef4e2ef0bf24
funkygao <funky.gao@gmail.com>
2013-10-10 09:56:05 +0800

    more modular

--- guard.go
+++ guard.go
@@ -25,9 +25,7 @@ package main
 
 import (
 	parser "github.com/funkygao/alsparser"
-	"github.com/funkygao/gofmt"
 	"path/filepath"
-	"runtime"
 	"sync"
 	"time"
 )
@@ -86,20 +84,4 @@ func guard(jsonConfig jsonConfig) {
 	parser.StopAll()
 
 	logger.Println("all lines scaned:", lines)
-}
-
-func runTicker(lines *int) {
-	ms := new(runtime.MemStats)
-	for _ = range ticker.C {
-		runtime.ReadMemStats(ms)
-		logger.Printf("goroutine: %d, mem: %s, lines: %d\n",
-			runtime.NumGoroutine(), gofmt.ByteSize(ms.Alloc), *lines)
-	}
-}
-
-func runAlarmCollector(ch <-chan parser.Alarm) {
-	alarmLogger := newAlarmLogger()
-	for alarm := range ch {
-		alarmLogger.Println(alarm)
-	}
 }
---
+++ watchdog.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	parser "github.com/funkygao/alsparser"
+	"github.com/funkygao/gofmt"
+	"runtime"
+)
+
+func runTicker(lines *int) {
+	ms := new(runtime.MemStats)
+	for _ = range ticker.C {
+		runtime.ReadMemStats(ms)
+		logger.Printf("goroutine: %d, mem: %s, lines: %d\n",
+			runtime.NumGoroutine(), gofmt.ByteSize(ms.Alloc), *lines)
+	}
+}
+
+func runAlarmCollector(ch <-chan parser.Alarm) {
+	alarmLogger := newAlarmLogger()
+	for alarm := range ch {
+		alarmLogger.Println(alarm)
+	}
+}
