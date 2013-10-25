1b6e4701ac5b3e84ce816eb1793d1d76a3a53683
funkygao <funky.gao@gmail.com>
2013-10-26 00:30:50 +0800

    _log_info now has more properties

--- parser/hist.go
+++ parser/hist.go
@@ -1,26 +1,27 @@
+// TODO
 package parser
 
 import (
-    "strings"
-    "time"
+	"strings"
+	"time"
 )
 
 type histEntry struct {
-    sum int64 //
-    duration time.Duration
+	sum      int64 //
+	duration time.Duration
 }
 
 // a complex KV data structure
 // key can be mixed up for sematics
-type hist map[string] histEntry
+type hist map[string]histEntry
 
 func newHist() hist {
-    return make(map[string] histEntry)
+	return make(map[string]histEntry)
 }
 
 func (this hist) Key(args ...string) string {
-    const keySeperator = ":"
-    return strings.Join(args, keySeperator)
+	const keySeperator = ":"
+	return strings.Join(args, keySeperator)
 }
 
 func (this hist) Avg() {
@@ -29,7 +30,7 @@ func (this hist) Avg() {
 func (this *hist) Reset() {
 }
 
-func (this *hist) Remember(entry histEntry)  {
+func (this *hist) Remember(entry histEntry) {
 }
 
 func (this hist) ShouldAlarm() {
--- parser/util.go
+++ parser/util.go
@@ -1,40 +1,48 @@
 package parser
 
 import (
-    json "github.com/bitly/go-simplejson"
-    "time"
+	json "github.com/bitly/go-simplejson"
+	"time"
 )
 
 func checkError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 type logInfo struct {
-    uid      int64
-    scriptId int64
-    serial   int
-    host     string
-    ip       string
+	uid          int64
+	snsid        string
+	level        int
+	payment_cash int
+	uri          string
+	scriptId     int64
+	serial       int
+	host         string
+	ip           string
 }
 
 // extract _log_info fields from a log data entry
 func extractLogInfo(data *json.Json) logInfo {
-    info := logInfo{}
+	info := logInfo{}
 
-    infoBody := data.Get("_log_info")
-    info.uid, _ = infoBody.Get("uid").Int64()
-    info.scriptId, _ = infoBody.Get("script_id").Int64()
-    info.serial, _ = infoBody.Get("serial").Int()
-    info.host, _ = infoBody.Get("host").String()
-    info.ip, _ = infoBody.Get("ip").String()
+	infoBody := data.Get("_log_info")
+	info.uid, _ = infoBody.Get("uid").Int64()
+	info.snsid, _ = infoBody.Get("snsid").String()
+	info.level, _ = infoBody.Get("level").Int()
+	info.payment_cash, _ = infoBody.Get("payment_cash").Int()
+	info.uri, _ = infoBody.Get("uri").String()
+	info.scriptId, _ = infoBody.Get("script_id").Int64()
+	info.serial, _ = infoBody.Get("serial").Int()
+	info.host, _ = infoBody.Get("host").String()
+	info.ip, _ = infoBody.Get("ip").String()
 
-    return info
+	return info
 }
 
 // timestamp of UTC to beijing time
 func tsToString(ts int) string {
-    t := time.Unix(int64(ts), 0)
-    return t.In(tzAjust).Format("01-02 15:04:05")
+	t := time.Unix(int64(ts), 0)
+	return t.In(tzAjust).Format("01-02 15:04:05")
 }
