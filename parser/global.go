1fc365bfa6ef18a2a058125216b76db9125f8cdd
funkygao <funky.gao@gmail.com>
2013-10-26 00:37:10 +0800

    refacetor

--- parser/global.go
+++ parser/global.go
@@ -55,3 +55,106 @@ const (
 
 	CONF_DIR = "conf/"
 )
+
+const (
+	Reset = "\x1b[0m"
+
+	Bright     = "\x1b[1m"
+	Dim        = "\x1b[2m"
+	Underscore = "\x1b[4m"
+	Blink      = "\x1b[5m"
+	Reverse    = "\x1b[7m"
+	Hidden     = "\x1b[8m"
+
+	FgBlack   = "\x1b[30m"
+	FgRed     = "\x1b[31m"
+	FgGreen   = "\x1b[32m"
+	FgYellow  = "\x1b[33m"
+	FgBlue    = "\x1b[34m"
+	FgMagenta = "\x1b[35m"
+	FgCyan    = "\x1b[36m"
+	FgWhite   = "\x1b[37m"
+
+	BgBlack   = "\x1b[40m"
+	BgRed     = "\x1b[41m"
+	BgGreen   = "\x1b[42m"
+	BgYellow  = "\x1b[43m"
+	BgBlue    = "\x1b[44m"
+	BgMagenta = "\x1b[45m"
+	BgCyan    = "\x1b[46m"
+	BgWhite   = "\x1b[47m"
+)
+
+const (
+	// error log
+	ERRLOG_CREATE_TABLE = `
+CREATE TABLE IF NOT EXISTS error (
+	area CHAR(10),
+	host CHAR(20),
+	ts INT,
+	cls VARCHAR(50),
+    level CHAR(20),
+    msg VARCHAR(200) NULL,
+    flash INT
+);
+`
+	ERRLOG_CREATE_INDEX = `
+CREATE INDEX IF NOT EXISTS err_idx ON error(ts, cls);
+`
+	ERRLOG_INSERT = "INSERT INTO error(area, ts, cls, level, msg, flash, host) VALUES(?,?,?,?,?,?,?)"
+
+	// payment log
+	PAYMENT_CREATE_TABLE = `
+CREATE TABLE IF NOT EXISTS payment (
+	area CHAR(10),
+	host CHAR(20),
+	ts INT,
+	type VARCHAR(50),
+    uid INT(10) NULL,
+    level INT,
+    amount INT,
+    ref VARCHAR(50) NULL,
+    item VARCHAR(40),
+    currency VARCHAR(20)
+);
+`
+	PAYMENT_CREATE_INDEX = `
+CREATE INDEX IF NOT EXISTS pay_idx ON payment(ts, type, area);
+`
+	PAYMENT_INSERT = "INSERT INTO payment(area, host, ts, type, uid, level, amount, ref, item, currency) VALUES(?,?,?,?,?,?,?,?,?,?)"
+
+	// slowresp log
+	SLOWRESP_CREATE_TABLE = `
+CREATE TABLE IF NOT EXISTS slowresp (
+	area CHAR(10),
+	host CHAR(20),
+	uri VARCHAR(50),
+	ts INT,
+	req_t INT,
+	db_t INT	
+);
+`
+	SLOWRESP_INSERT = `INSERT INTO slowresp(area, host, uri, ts, req_t, db_t) VALUES(?,?,?,?,?,?)`
+
+	// level up log
+	LEVELUP_CREATE_TABLE = `
+CREATE TABLE IF NOT EXISTS levelup (
+	area CHAR(10),	
+	ts INT,
+	fromlevel INT
+);
+`
+	LEVELUP_INSERT = `INSERT INTO levelup(area, ts, fromlevel) VALUES(?,?,?)`
+
+	PHPERROR_CREATE_TABLE = `
+CREATE TABLE IF NOT EXISTS phperror (
+	area CHAR(10),	
+	ts INT,
+	host CHAR(25),
+	level CHAR(15),
+	src_file VARCHAR(80),
+	msg VARCHAR(100)
+);
+`
+	PHPERROR_INSERT = `INSERT INTO phperror(area, ts, host, level, src_file, msg) VALUES(?,?,?,?,?,?)`
+)
