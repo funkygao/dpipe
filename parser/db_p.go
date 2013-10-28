70f51e5dc52cdfadf802a33aa6b232b124271d90
funkygao <funky.gao@gmail.com>
2013-10-28 09:57:08 +0800

    try fixing sqlite3 table is locked err

--- parser/db_p.go
+++ parser/db_p.go
@@ -58,7 +58,9 @@ func (this *DbParser) createDB(createTable string, dbFile string) {
 	// performance tuning for sqlite3
 	_, err = this.db.Exec("PRAGMA synchronous = OFF")
 	checkError(err)
-	_, err = this.db.Exec("PRAGMA journal_mode = MEMORY")
+	_, err = this.db.Exec("PRAGMA journal_mode = OFF")
+	checkError(err)
+	_, err = this.db.Exec("PRAGMA read_uncommitted = true")
 	checkError(err)
 }
 
