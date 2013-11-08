a412d364868ea53c20b70e43230d95f5fb6e5aad
funkygao <funky.gao@gmail.com>
2013-11-09 00:03:19 +0800

    host ended log line parser

---
+++ parser/hostline.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"github.com/funkygao/alser/config"
+	"path/filepath"
+	"strings"
+)
+
+// area,ts,....,hostIp
+type HostLineParser struct {
+	AlsParser
+}
+
+func newHostLineParser(conf *config.ConfParser, chUpstream chan<- Alarm, chDownstream chan<- string) (this *HostLineParser) {
+	this = new(HostLineParser)
+	this.init(conf, chUpstream, chDownstream)
+
+	return
+}
+
+func (this *HostLineParser) ParseLine(line string) (area string, ts uint64, msg string) {
+	area, ts, msg = this.AlsParser.ParseLine(line)
+
+	parts := strings.Split(msg, ",")
+	n := len(parts)
+	data, host := parts[n-1], strings.Join(parts[:n-1], ",")
+
+	this.colorPrintfLn("%3s %15s %s", area, host, data)
+	this.beep()
+
+	return
+}
