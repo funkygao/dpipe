1190cc04f5ef0231ec19462d2d245e73e7308413
funkygao <funky.gao@gmail.com>
2013-11-12 22:28:14 +0800

    avoid hard coding

--- flashlog.go
+++ flashlog.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 /*
@@ -20,18 +20,18 @@ import (
 +-------------+---------------------+------+-----+---------+----------------+
 */
 func flashlogDataSource() {
-    db, err := sql.Open("mysql", "flashlog:flashlog@unix(/var/run/mysqld/mysqld.sock)/flashlog?charset=utf8")
-    if err != nil {
-        panic(err)
-        return
-    }
+	db, err := sql.Open(MYSQL_DRIVER, FLASHLOG_DSN)
+	if err != nil {
+		panic(err)
+		return
+	}
 
-    rows, err := db.Query("select * from log_us WHERE type=299 ORDER BY ID limit 10")
-    if err != nil {
-        panic(err)
-    }
+	rows, err := db.Query("select * from log_us WHERE type=299 ORDER BY ID limit 10")
+	if err != nil {
+		panic(err)
+	}
 
-    for rows.Next() {
-        fmt.Println(rows)
-    }
+	for rows.Next() {
+		fmt.Println(rows)
+	}
 }
