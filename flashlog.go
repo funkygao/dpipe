c82e168001f965822ff3ffe3590d98895f7a3a44
funkygao <funky.gao@gmail.com>
2013-11-12 19:31:32 +0800

    schema

--- flashlog.go
+++ flashlog.go
@@ -6,6 +6,19 @@ import (
     _ "github.com/go-sql-driver/mysql"
 )
 
+/*
++-------------+---------------------+------+-----+---------+----------------+
+| Field       | Type                | Null | Key | Default | Extra          |
++-------------+---------------------+------+-----+---------+----------------+
+| id          | bigint(20) unsigned | NO   | PRI | NULL    | auto_increment |
+| uid         | bigint(20) unsigned | NO   | MUL | NULL    |                |
+| type        | int(10) unsigned    | NO   | MUL | NULL    |                |
+| data        | blob                | NO   |     | NULL    |                |
+| ip          | bigint(20)          | NO   | MUL | NULL    |                |
+| ua          | int(10) unsigned    | NO   | MUL | NULL    |                |
+| date_create | int(10) unsigned    | NO   | MUL | NULL    |                |
++-------------+---------------------+------+-----+---------+----------------+
+*/
 func flashlogDataSource() {
     db, err := sql.Open("mysql", "flashlog:flashlog@unix(/var/run/mysqld/mysqld.sock)/flashlog?charset=utf8")
     if err != nil {
