7eb39f309996dc8c75a0416b14313c05fc42051c
funkygao <funky.gao@gmail.com>
2013-11-14 16:19:13 +0800

    can basically work

--- worker_db.go
+++ worker_db.go
@@ -1,35 +1,16 @@
 package main
 
 import (
-	"database/sql"
+	"bytes"
+	"compress/zlib"
 	"fmt"
+	json "github.com/bitly/go-simplejson"
 	"github.com/funkygao/alser/config"
 	sqldb "github.com/funkygao/alser/db"
 	"github.com/funkygao/alser/parser"
-	_ "github.com/go-sql-driver/mysql"
 	"sync"
+	"time"
 )
-
-type DbWorker struct {
-	Worker
-}
-
-func newDbWorker(id int,
-	dataSource string, conf config.ConfGuard, tailMode bool,
-	wg *sync.WaitGroup, mutex *sync.Mutex,
-	chLines chan<- int, chAlarm chan<- parser.Alarm) Runnable {
-	this := new(DbWorker)
-	this.Worker = Worker{id: id,
-		dataSource: dataSource, conf: conf, tailMode: tailMode,
-		wg: wg, Mutex: mutex,
-		chLines: chLines, chAlarm: chAlarm}
-	return this
-}
-
-func (this *DbWorker) Run() {
-	defer this.Done()
-
-}
 
 /*
 +-------------+---------------------+------+-----+---------+----------------+
@@ -44,19 +25,107 @@ func (this *DbWorker) Run() {
 | date_create | int(10) unsigned    | NO   | MUL | NULL    |                |
 +-------------+---------------------+------+-----+---------+----------------+
 */
-func flashlogDataSource() {
-	db, err := sql.Open(sqldb.DRIVER_MYSQL, FLASHLOG_DSN)
-	if err != nil {
-		panic(err)
-		return
+type DbWorker struct {
+	Worker
+	Lines chan string
+	db    *sqldb.SqlDb
+}
+
+func newDbWorker(id int,
+	dataSource string, conf config.ConfGuard, tailMode bool,
+	wg *sync.WaitGroup, mutex *sync.Mutex,
+	chLines chan<- int, chAlarm chan<- parser.Alarm) Runnable {
+	this := new(DbWorker)
+	this.Worker = Worker{id: id,
+		dataSource: dataSource, conf: conf, tailMode: tailMode,
+		wg: wg, Mutex: mutex,
+		chLines: chLines, chAlarm: chAlarm}
+	this.Lines = make(chan string)
+	this.db = sqldb.NewSqlDb(sqldb.DRIVER_MYSQL, FLASHLOG_DSN, logger)
+	return this
+}
+
+func (this *DbWorker) Run() {
+	defer this.Done()
+
+	go this.feedLines()
+
+	for line := range this.Lines {
+		// a valid line scanned
+		this.chLines <- 1
+
+		// feed the parsers one by one
+		for _, parserId := range this.conf.Parsers {
+			parser.Dispatch(parserId, line)
+		}
 	}
 
-	rows, err := db.Query("select * from log_us WHERE type=299 ORDER BY ID limit 10")
-	if err != nil {
+	if options.verbose {
+		logger.Printf("%s finished\n", *this)
+	}
+
+}
+
+func (this *DbWorker) feedLines() {
+	var lastId int64 = this.getLastId()
+	var query string
+	for {
+		time.Sleep(10)
+
+		var (
+			id   int64
+			typ  int
+			data string
+		)
+
+		query = fmt.Sprintf("SELECT id,type,data FROM %s WHERE id>=%d ORDER BY id", this.dataSource, lastId)
+
+		rows := this.db.Query(query)
+		for rows.Next() {
+			if err := rows.Scan(&id, &typ, &data); err != nil {
+				panic(err)
+			}
+
+			if line := this.genLine(typ, data); line != "" {
+				this.Lines <- line
+			}
+		}
+
+		lastId = id
+	}
+}
+
+func (this *DbWorker) getLastId() (lastId int64) {
+	row := this.db.QueryRow(fmt.Sprintf("SELECT max(id) from %s", this.dataSource))
+	if err := row.Scan(&lastId); err != nil {
 		panic(err)
 	}
 
-	for rows.Next() {
-		fmt.Println(rows)
+	return
+}
+
+func (this *DbWorker) genLine(typ int, data string) string {
+	// gzuncompress data
+	b := bytes.NewBufferString(data)
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		panic(err)
 	}
+	var d []byte
+	if _, err := r.Read(d); err != nil {
+		panic(err)
+	}
+
+	var jsonData *json.Json
+	var e error
+	if jsonData, e = json.NewJson(d); e != nil {
+		logger.Printf("unkown feed: %s\n", data)
+		return ""
+	}
+
+	fmt.Println(*jsonData)
+
+	fmt.Println(string(d))
+
+	return ""
 }
