ca1637c3556a775331dbf0c6ff1207757d0b7951
funkygao <funky.gao@gmail.com>
2013-11-23 08:50:40 +0800

    fix fatal bug of alarm mail interval
    in the past, will send mail only after 5 minutes when no
    new alarm came in(only when it's peaceful will it report alarms)

--- parser/alarm.go
+++ parser/alarm.go
@@ -13,41 +13,26 @@ type Alarm interface {
 	String() string
 }
 
-func runSendAlarmsWatchdog(conf *config.Config) {
+func sendAlarmMails(conf *config.Config, mailBody *string, bodyLines *int) {
 	const mailTitlePrefix = "ALS Alarm"
-	mailBody := ""
-	bodyLines := 0
 	mailTo := conf.String("mail.guarded", "")
 	if mailTo == "" {
 		panic("empty mail.guarded")
 	}
+
 	mailSleep := conf.Int("mail.sleep_start", 120)
 	backoffThreshold := conf.Int("mail.backoff_threshold", 10)
 	maxSleep, minSleep, sleepStep := conf.Int("mail.sleep_max", mailSleep*2),
 		conf.Int("mail.sleep_min", mailSleep/2), conf.Int("mail.sleep_step", 5)
-
 	for {
 		select {
-		case line, ok := <-chParserAlarm:
-			if !ok {
-				// chParserAlarm closed, this should never happen
-				break
-			}
-
-			if debug {
-				logger.Printf("got alarm: %s\n", line)
-			}
-
-			mailBody += gotime.TsToString(int(time.Now().UTC().Unix())) + " " + line + "\n"
-			bodyLines += 1
-
 		case <-time.After(time.Second * time.Duration(mailSleep)):
-			if mailBody != "" {
-				go mail.Sendmail(mailTo, fmt.Sprintf("%s - %d", mailTitlePrefix, bodyLines), mailBody)
+			if *mailBody != "" {
+				go mail.Sendmail(mailTo, fmt.Sprintf("%s - %d", mailTitlePrefix, *bodyLines), *mailBody)
 				logger.Printf("alarm sent=> %s, sleep=%d\n", mailTo, mailSleep)
 
 				// backoff sleep
-				if bodyLines >= backoffThreshold {
+				if *bodyLines >= backoffThreshold {
 					// busy alarm
 					mailSleep -= sleepStep
 					if mailSleep < minSleep {
@@ -61,10 +46,26 @@ func runSendAlarmsWatchdog(conf *config.Config) {
 					}
 				}
 
-				mailBody = ""
-				bodyLines = 0
+				*mailBody = ""
+				*bodyLines = 0
 			}
-
 		}
 	}
+}
+
+func runSendAlarmsWatchdog(conf *config.Config) {
+	mailBody := ""
+	bodyLines := 0
+
+	go sendAlarmMails(conf, &mailBody, &bodyLines)
+
+	for line := range chParserAlarm {
+		if debug {
+			logger.Printf("got alarm: %s\n", line)
+		}
+
+		mailBody += gotime.TsToString(int(time.Now().UTC().Unix())) + " " + line + "\n"
+		bodyLines += 1
+	}
+
 }
