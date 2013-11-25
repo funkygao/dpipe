74ef2ed49c903e24318cb2403cca2b8e7f58dbb3
funkygao <funky.gao@gmail.com>
2013-11-25 09:30:05 +0800

    beep

--- parser/jsoncollector.go
+++ parser/jsoncollector.go
@@ -46,10 +46,8 @@ func (this *JsonCollectorParser) ParseLine(line string) (area string, ts uint64,
 
 	if this.conf.InstantFormat != "" {
 		iargs := append([]interface{}{area}, args...) // 'area' is always 1st col
+		this.beep()
 		this.colorPrintfLn(this.conf.InstantFormat, iargs...)
-		if this.conf.BeepThreshold > 0 {
-			this.beep()
-		}
 	}
 
 	// insert_stmt must be like INSERT INTO (area, ts, ...)
