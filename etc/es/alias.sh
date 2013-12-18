8c1b85525259fe243b60f0094f587d8969fdba72
funkygao <funky.gao@gmail.com>
2013-12-18 17:07:40 +0800

    alias for ffs and rs

--- etc/es/alias.sh
+++ etc/es/alias.sh
@@ -2,9 +2,17 @@ curl localhost:9200/_aliases?pretty
 
 curl -XPOST localhost:9200/_aliases -d '{
     "actions": [{
-        "add": {
+        "remove": {
             "index": "rs_2013_12",
             "alias": "rs_latest"
+        },
+        "add": {
+            "index": "fun_rs_2013_12",
+            "alias": "rs_latest"
+        },
+        "add": {
+            "index": "fun_ffs_2013_12",
+            "alias": "ffs_latest"
         }
     }]
 }'
