e0e9a97b4ef081c7859d8e9a0e9a037c03bb626b
funkygao <funky.gao@gmail.com>
2013-12-19 15:56:34 +0800

    es index alias

--- etc/es/alias.sh
+++ etc/es/alias.sh
@@ -9,7 +9,12 @@ curl -XPOST localhost:9200/_aliases -d '{
         "add": {
             "index": "fun_rs_2013_12",
             "alias": "rs_latest"
-        },
+        }
+    }]
+}'
+
+curl -XPOST localhost:9200/_aliases -d '{
+    "actions": [{
         "add": {
             "index": "fun_ffs_2013_12",
             "alias": "ffs_latest"
