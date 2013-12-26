47d1b72b5689afbc8f1bc69f33e1c470b26ac862
funkygao <funky.gao@gmail.com>
2013-12-26 17:20:26 +0800

    const

---
+++ rule/const.go
@@ -0,0 +1,21 @@
+package rule
+
+const (
+	FIELD_TYPE_STRING   = "string" // default type
+	FIELD_TYPE_IP       = "ip"
+	FIELD_TYPE_FLOAT    = "float"
+	FIELD_TYPE_INT      = "int"
+	FIELD_TYPE_MONEY    = "money"
+	FIELD_TYPE_LEVEL    = "level"
+	FIELD_TYPE_BASEFILE = "base_file"
+
+	FIELD_NAME_CURRENCY = "currency"
+	FIELD_NAME_TYPE     = "typ"
+	FIELD_NAME_IP       = "ip"
+
+	DATASOURCE_DB   = "db"
+	DATASOURCE_FILE = "file"
+	DATASOURCE_SYS  = "sys"
+
+	INDEX_YEARMONTH = "@ym"
+)
