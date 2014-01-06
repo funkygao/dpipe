84340b36ee0d20fee02e0ccf00885b279e9e2e28
funkygao <funky.gao@gmail.com>
2014-01-06 11:32:55 +0800

    batch als log loader input

--- plugins/batchalslog_input.go
+++ plugins/batchalslog_input.go
@@ -1,24 +1,147 @@
 package plugins
 
 import (
+	"github.com/funkygao/als"
 	"github.com/funkygao/funpipe/engine"
 	conf "github.com/funkygao/jsconf"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
 )
 
 type BatchAlsLogInput struct {
-	checkpointFile string
+	runner      engine.InputRunner
+	e           *engine.EngineConfig
+	chkpnt      *als.FileCheckpoint
+	workerNChan chan int
+	rootDir     string
+	workersWg   *sync.WaitGroup
+	excepts     []string
+	project     string
+	sink        int
 }
 
 func (this *BatchAlsLogInput) Init(config *conf.Conf) {
-	this.checkpointFile = config.String("chkpntfile", "")
+	this.rootDir = config.String("root_dir", "")
+	this.sink = config.Int("sink", 0)
+	this.workerNChan = make(chan int, config.Int("concurrent_num", 20))
+	this.chkpnt = als.NewFileCheckpoint(config.String("chkpntfile", ""))
+	this.project = config.String("proj", "rs")
+	this.excepts = config.StringList("except", nil)
+}
 
+func (this *BatchAlsLogInput) CleanupForRestart() {
+	this.chkpnt.Dump()
 }
 
 func (this *BatchAlsLogInput) Run(r engine.InputRunner, e *engine.EngineConfig) error {
+	this.runner = r
+	this.e = e
+
+	this.chkpnt.Load()
+	ticker := time.NewTicker(time.Second * 10)
+	go func() {
+		for _ = range ticker.C {
+			this.chkpnt.Dump()
+		}
+	}()
+
+	this.workersWg = new(sync.WaitGroup)
+
+	filepath.Walk(this.rootDir, this.runSingleLogfile)
+
+	// wait for all workers done
+	this.workersWg.Wait()
+	ticker.Stop()
+	this.chkpnt.Dump()
+
+	engine.Globals().Printf("%s done", r.Name())
+
 	return nil
 }
 
 func (this *BatchAlsLogInput) Stop() {
+
+}
+
+func (this *BatchAlsLogInput) shouldRunSingleLogfile(path string) bool {
+	if this.chkpnt.Contains(path) {
+		return false
+	}
+
+	for _, ex := range this.excepts {
+		if strings.HasPrefix(filepath.Base(path), ex) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (this *BatchAlsLogInput) runSingleLogfile(path string, f os.FileInfo, err error) (e error) {
+	if f == nil || f.IsDir() || !this.shouldRunSingleLogfile(path) {
+		return
+	}
+
+	// limit concurrent workers
+	this.workerNChan <- 1
+	go this.doRunSingleLogfile(path)
+
+	return
+}
+
+func (this *BatchAlsLogInput) doRunSingleLogfile(path string) {
+	this.workersWg.Add(1)
+
+	reader := als.NewAlsReader(path)
+
+	defer func() {
+		reader.Close()
+		this.chkpnt.Put(path)
+		this.chkpnt.Dump()
+		this.workersWg.Done()
+
+		<-this.workerNChan // release the lock
+	}()
+
+	var (
+		line    []byte
+		lineN   int
+		inChan  = this.runner.InChan()
+		err     error
+		pack    *engine.PipelinePack
+		project = this.e.Project(this.project)
+		globals = engine.Globals()
+	)
+
+LOOP:
+	for {
+		line, err = reader.ReadLine()
+		switch err {
+		case nil:
+			lineN += 1
+			if globals.Verbose {
+				project.Printf("running %s, lineNum: %d\n", path, lineN)
+			}
+
+			pack = <-inChan
+			if err = pack.Message.FromLine(string(line)); err != nil {
+				project.Printf("[%s]error line: %v <= %s\n", path, err, string(line))
+
+				pack.Recycle()
+				continue
+			}
+
+			pack.Message.Sink = this.sink
+			this.runner.Inject(pack)
+
+		case io.EOF:
+			break LOOP
+		}
+	}
 
 }
 
