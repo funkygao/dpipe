c2bcc9efc4f23d9aad84e7691d1aa6d747e378a9
funkygao <funky.gao@gmail.com>
2014-01-08 10:06:54 +0800

    project conf has more fields

--- engine/project.go
+++ engine/project.go
@@ -7,20 +7,37 @@ import (
 	"os"
 )
 
+type projectEmailConf struct {
+	Recipients                                string
+	BackoffThreshold                          int
+	SleepStart, SleepStep, SleepMax, SleepMin int
+	LineThreshold                             int
+}
+
 type ConfProject struct {
 	*log.Logger
 
 	Name        string `json:"name"`
 	IndexPrefix string `json:"index_prefix"`
 
-	// such as a@a.com,b@b.com
-	AlarmMailTo string `json:"alarm_to"`
+	MailConf projectEmailConf
 }
 
 func (this *ConfProject) FromConfig(c *conf.Conf) {
 	this.Name = c.String("name", "")
 	this.IndexPrefix = c.String("index_prefix", this.Name)
-	this.AlarmMailTo = c.String("alarm_to", "")
+	mailSection, err := c.Section("alarm_email")
+	if err != nil {
+		panic(err)
+	}
+	this.MailConf = projectEmailConf{}
+	this.MailConf.Recipients = mailSection.String("recipients", "")
+	this.MailConf.LineThreshold = mailSection.Int("line_threshold", 10)
+	this.MailConf.BackoffThreshold = mailSection.Int("backoff_threshold", 15)
+	this.MailConf.SleepStart = mailSection.Int("sleep_start", 600)
+	this.MailConf.SleepMin = mailSection.Int("sleep_min", 240)
+	this.MailConf.SleepMax = mailSection.Int("sleep_max", 1600)
+	this.MailConf.SleepStep = mailSection.Int("sleep_step", 60)
 
 	logfile := c.String("logfile", "var/"+this.Name+".log")
 	logWriter, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
