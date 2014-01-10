e46c632e646fed256331251c71b69b6550f6b547
funkygao <funky.gao@gmail.com>
2014-01-10 16:17:33 +0800

    todo

---
+++ plugins/stats_filter.go
@@ -0,0 +1,51 @@
+package plugins
+
+import (
+	"github.com/funkygao/funpipe/engine"
+	conf "github.com/funkygao/jsconf"
+)
+
+// pv, pv latency and the alike statistics before feeding ES
+type StatsFilter struct {
+	blackhole bool
+}
+
+func (this *StatsFilter) Init(config *conf.Conf) {
+	this.blackhole = config.Bool("blackhole", false)
+}
+
+func (this *StatsFilter) Run(r engine.FilterRunner, h engine.PluginHelper) error {
+	globals := engine.Globals()
+	if globals.Verbose {
+		globals.Printf("[%s] started\n", r.Name())
+	}
+
+	var (
+		pack   *engine.PipelinePack
+		ok     = true
+		inChan = r.InChan()
+	)
+
+	for ok && !globals.Stopping {
+		select {
+		case pack, ok = <-inChan:
+			if !ok {
+				break
+			}
+
+			if !this.blackhole {
+				globals.Printf("got msg: %s\n", pack.Message.RawLine())
+			}
+
+			pack.Recycle()
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	engine.RegisterPlugin("StatsFilter", func() engine.Plugin {
+		return new(StatsFilter)
+	})
+}
