e53187317e4a85818af3b1d341e8a7b656c0ec18
funkygao <funky.gao@gmail.com>
2014-01-15 20:43:23 +0800

    move place

--- cmd/dpiped/option.go
+++ cmd/dpiped/option.go
@@ -39,13 +39,6 @@ func showUsage() {
 	flag.PrintDefaults()
 }
 
-func showVersionAndExit() {
-	fmt.Fprintf(os.Stderr, "%s %s (build: %s)\n", os.Args[0], VERSION, BuildID)
-	fmt.Fprintf(os.Stderr, "Built with %s %s for %s/%s\n",
-		runtime.Compiler, runtime.Version(), runtime.GOOS, runtime.GOARCH)
-	os.Exit(0)
-}
-
 func setupMaxProcsAndProfiler() {
 	numCpu := runtime.NumCPU()
 	maxProcs := numCpu/2 + 1
