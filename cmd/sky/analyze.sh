a0be5dab0025acb5bc226068590e143a0a35df60
funkygao <funky.gao@gmail.com>
2014-01-16 14:20:20 +0800

    sky api

--- cmd/sky/analyze.sh
+++ cmd/sky/analyze.sh
@@ -2,6 +2,11 @@
 #=================================
 # rs_user
 #=================================
+
+curl -X GET http://localhost:8585/tables/rs_user
+
+curl -X GET http://localhost:8585/tables/rs_user/stats
+
 curl -X POST http://localhost:8585/tables/rs_user/query -d '{
     "steps": [
         {"type":"visit","fields":[{"name":"count","expression":"count()"}]}
