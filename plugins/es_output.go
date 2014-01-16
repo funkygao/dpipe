db3ee91d37124f9a253b6ec61b2b164e1d9009f9
funkygao <funky.gao@gmail.com>
2014-01-16 14:47:20 +0800

    wider

--- plugins/es_output.go
+++ plugins/es_output.go
@@ -94,7 +94,7 @@ func (this *EsOutput) Run(r engine.OutputRunner, h engine.PluginHelper) error {
 func (this *EsOutput) handlePeriodicalCounters() {
 	globals := engine.Globals()
 	for name, n := range this.counters {
-		globals.Printf("sink to ES %20s %8d", name, n)
+		globals.Printf("sink to ES %30s %8d", name, n)
 
 		this.counters[name] = 0
 	}
