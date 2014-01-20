517cb39c39cce294d1474cb212fc84d1f91c8f69
funkygao <funky.gao@gmail.com>
2014-01-20 18:31:34 +0800

    only report positive number

--- plugins/alslog_input.go
+++ plugins/alslog_input.go
@@ -152,7 +152,9 @@ func (this *AlsLogInput) Run(r engine.InputRunner, h engine.PluginHelper) error 
 func (this *AlsLogInput) handlePeriodicalCounters() {
 	globals := engine.Globals()
 	for ident, n := range this.counters {
-		globals.Printf("%-15s %8d", ident, n)
+		if n > 0 {
+			globals.Printf("%-15s %8d", ident, n)
+		}
 
 		this.counters[ident] = 0
 	}
