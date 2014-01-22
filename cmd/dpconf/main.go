6d8460c41b11d1197c2bd155eff1574062c40f04
funkygao <funky.gao@gmail.com>
2014-01-22 21:07:01 +0800

    visualization and validator done

--- cmd/dpconf/main.go
+++ cmd/dpconf/main.go
@@ -4,17 +4,24 @@ import (
 	"flag"
 	"fmt"
 	conf "github.com/funkygao/jsconf"
-	"github.com/funkygao/pretty"
 	"os"
+	"time"
 )
 
 const (
-	IDENT = "ident"
-	MATCH = "match"
+	IDENT    = "ident"
+	MATCH    = "match"
+	NAME     = "name"
+	DISABLED = "disabled"
 )
 
+type identEntry struct {
+	disabled bool
+	matches  []string
+}
+
 var (
-	graph map[string][]string
+	graph map[string]*identEntry
 )
 
 func main() {
@@ -24,12 +31,13 @@ func main() {
 
 	cf, err := conf.Load(fn)
 	if err != nil {
-		fmt.Printf("Invalid config file[%s]: %v", fn, err)
+		fmt.Printf("Invalid config file[%s]: %v\n", fn, err)
 		os.Exit(1)
 	}
 
-	graph = make(map[string][]string)
+	graph = make(map[string]*identEntry)
 
+	// only visualize the plugins section
 	for i := 0; i < len(cf.List("plugins", nil)); i++ {
 		section, err := cf.Section(fmt.Sprintf("plugins[%d]", i))
 		if err != nil {
@@ -42,11 +50,37 @@ func main() {
 	showGraph()
 }
 
-// recursive
+// recursively
 func handleSection(section *conf.Conf) {
 	ident := section.String(IDENT, "")
 	if ident != "" {
-		graph[ident] = make([]string, 0, 10)
+		if _, present := graph[ident]; present {
+			fmt.Printf("ident[%s]duplicated\n", ident)
+			os.Exit(1)
+		}
+
+		ie := &identEntry{}
+		ie.matches = make([]string, 0, 10)
+		ie.disabled = section.Bool(DISABLED, false)
+
+		graph[ident] = ie
+	}
+
+	matches := section.StringList(MATCH, nil)
+	if matches != nil {
+		pluginName := section.String(NAME, "")
+		if pluginName == "" {
+			fmt.Printf("plugin match %v has no 'name' key\n", matches)
+			os.Exit(1)
+		}
+
+		for _, id := range matches {
+			if _, present := graph[id]; present {
+				graph[id].matches = append(graph[id].matches, pluginName)
+			} else {
+				fmt.Printf("%15s -> %s\n", id, pluginName)
+			}
+		}
 	}
 
 	sub := section.Interface("", nil).(map[string]interface{})
@@ -54,38 +88,35 @@ func handleSection(section *conf.Conf) {
 		return
 	}
 
-	if sub != nil {
-		for k, v := range sub {
-			if x, ok := v.([]interface{}); ok {
+	for k, v := range sub {
+		if x, ok := v.([]interface{}); ok {
+			switch x[0].(type) {
+			case string, float64, int, bool, time.Time:
+				// this section will never find 'ident'
+				continue
+			}
 
-				switch x[0].(type) {
-				case string:
+			for i := 0; i < len(section.List(k, nil)); i++ {
+				key := fmt.Sprintf("%s[%d]", k, i)
+				sec, err := section.Section(key)
+				if err != nil {
 					continue
-				case float64:
-					continue
 				}
 
-				if _, ss := x[0].(string); ss {
-					//continue
-				}
-				fmt.Printf("%s => %T\n", k, v)
-
-				for i := 0; i < len(section.List(k, nil)); i++ {
-					key := fmt.Sprintf("%s[%d]", k, i)
-					fmt.Println(key)
-					sec, err := section.Section(key)
-					if err != nil {
-						continue
-					}
-
-					handleSection(sec)
-				}
-
+				handleSection(sec)
 			}
 		}
 	}
 }
 
 func showGraph() {
-	pretty.Printf("%# v", graph)
+	fmt.Println()
+	for ident, ie := range graph {
+		var flag = "△"
+		if ie.disabled {
+			flag = "▼"
+		}
+
+		fmt.Printf("%15s[%s] -> %v\n", ident, flag, ie.matches)
+	}
 }
