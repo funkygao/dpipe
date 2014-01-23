9cbaad9df12a72acd155dc87429c4ad77c9e1cea
funkygao <funky.gao@gmail.com>
2014-01-23 12:43:33 +0800

    functional

--- cmd/sky/analyze.sh
+++ cmd/sky/analyze.sh
@@ -7,8 +7,43 @@ curl -X GET http://localhost:8585/tables/rs_user
 
 curl -X GET http://localhost:8585/tables/rs_user/stats
 
-curl -X POST http://localhost:8585/tables/rs_user/query -d '{
+curl -X POST http://localhost:8585/tables/rs_click/query -d '{
     "steps": [
-        {"type":"visit","fields":[{"name":"count","expression":"count()"}]}
+        {
+            "type": "selection",
+            "name": "0",
+            "dimensions": ["ver", "action"],
+            "fields":[
+                {
+                    "name":"count",
+                    "expression":"count()"
+                }
+            ]
+        }
     ]
 }'
+
+curl -s -XPOST localhost:8585/tables/rs_click/query -d '{
+    "sessionIdleTime": 7200,
+    "steps": [
+        {
+            "type": "condition", 
+            "expression": "true",
+            "within": [0, 0],
+            "steps": [
+                {
+                    "type": "selection",
+                    "name": "0",
+                    "dimensions": ["action"],
+                    "fields": [
+                        {
+                            "name": "count",
+                            "expression": "count()"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}'
+
