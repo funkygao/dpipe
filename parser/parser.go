529198f1d35e70308660d9f09fcafd0d3ea3feab
funkygao <funky.gao@gmail.com>
2014-01-27 10:36:46 +0800

    ad-hoc parser

---
+++ parser/parser.go
@@ -0,0 +1,27 @@
+/*
+Parser for some special messages
+*/
+package parser
+
+import (
+	"errors"
+)
+
+type ParseMsg func(msg string) string
+
+var (
+	ErrInvaidParser = errors.New("invalid parser type")
+
+	allParsers = map[string]ParseMsg{
+		"syslogngStats": parseSyslogNgStats,
+	}
+)
+
+func Parse(typ string, msg string) (string, error) {
+	parse, present := allParsers[typ]
+	if !present {
+		return "", ErrInvaidParser
+	}
+
+	return parse(msg), nil
+}
