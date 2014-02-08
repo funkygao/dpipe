cfb3daf743050bb3f85a884d6a69635628288037
funkygao <funky.gao@gmail.com>
2014-02-08 19:58:06 +0800

    test

--- plugins/alarm_worker_test.go
+++ plugins/alarm_worker_test.go
@@ -1,7 +1,9 @@
 package plugins
 
 import (
+	"github.com/funkygao/als"
 	"github.com/funkygao/assert"
+	conf "github.com/funkygao/jsconf"
 	"testing"
 )
 
@@ -13,4 +15,16 @@ func TestNormalize(t *testing.T) {
 	msg = "user id: 34343434 adfasf"
 	m := normalizers["digit"].ReplaceAll([]byte(msg), []byte("?"))
 	assert.Equal(t, "user id: ? adfasf", string(m))
+}
+
+func TestAlarmFieldIgnore(t *testing.T) {
+	f := alarmWorkerConfigField{}
+	c, _ := conf.Load("fixture/ignore.cf")
+	f.init(c)
+	msg := als.NewAlsMessage()
+	line := `ae,1391857296,{"host":"172.31.32.91","msg":"2014-02-08 06:01:35 [INFO] Update local version to e9818f0812b3933ac62d630b5b99aac5 for release royal.ae.php"}`
+	msg.FromLine(line)
+	value, err := f.value(msg)
+	assert.Equal(t, nil, err)
+	t.Logf("%v %v", value, err)
 }
