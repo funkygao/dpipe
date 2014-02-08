0ddaf8dd8d0ac3dc8f8a39ae18b52ab39b88ed70
funkygao <funky.gao@gmail.com>
2014-02-08 22:38:54 +0800

    more test cases

--- plugins/alarm_worker_test.go
+++ plugins/alarm_worker_test.go
@@ -21,9 +21,17 @@ func TestAlarmFieldIgnore(t *testing.T) {
 	f := alarmWorkerConfigField{}
 	c, _ := conf.Load("fixture/ignore.cf")
 	f.init(c)
+	assert.Equal(t, 3, len(f._regexIgnores))
+	assert.Equal(t, 3, len(f.ignores))
+	assert.Equal(t, "msg", f.name)
+	assert.Equal(t, "syslogngStats", f.parser)
+	assert.Equal(t, als.KEY_TYPE_STRING, f.typ)
+	assert.Equal(t, 0, len(f.normalizers))
+
 	msg := als.NewAlsMessage()
 	line := `ae,1391857296,{"host":"172.31.32.91","msg":"2014-02-08 06:01:35 [INFO] Update local version to e9818f0812b3933ac62d630b5b99aac5 for release royal.ae.php"}`
 	msg.FromLine(line)
+	assert.Equal(t, "ae", msg.Area)
 	value, err := f.value(msg)
 	assert.Equal(t, nil, err)
 	t.Logf("%v %v", value, err)
